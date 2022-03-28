6c6fc0b53a36b7e4c0ddf34c8df8f869e569e4d6
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2022-03-28 19:10:50 +0900

    represented main thm

--- exp.v
+++ exp.v
@@ -3,6 +3,8 @@ From Coq Require Import Notations.
 Require Import String.
 Require Import Arith.
 From Coq Require Import DecimalString.
+Require Import List.
+Import ListNotations.
 
 Section Expression_reasoning.
 Inductive exp : Set :=
@@ -41,29 +43,31 @@ Context `{FSL : finmap string loc}.
 Context `{FLV : finmap loc val}.
 
 Reserved Notation
-         "s '/' m '=[' e ']=>' v '/' m'"
-         (at level 40, m at next level, e at next level, v at next level, m' at next level).
-Inductive comp : env -> mem -> string -> val -> mem -> Prop :=
-| CInt s m e n : int_of_string e = Some n -> s/m =[e]=> n/m
-| CVar s m x l v : s x = Some l -> m l = Some v -> s/m =[x]=> v/m
-| CRec s m x e l v m' : s/m =[e]=> v/m' -> m l = None -> m' l = None
+         "s '/' m '=[' e ']=>' v '/' m' '*' l"
+         (at level 40, m at next level, e at next level, v at next level, m' at next level, l at next level).
+
+Inductive comp : env -> mem -> string -> val -> mem -> list loc -> Prop :=
+| CInt s m e n : int_of_string e = Some n -> s/m =[e]=> n/m*nil
+| CVar s m x l v : s x = Some l -> m l = Some v -> s/m =[x]=> v/m*[l]
+| CRec s m x e l v m' ls : s/m =[e]=> v/m'*ls -> m l = None -> m' l = None
                         -> s/m =[("{" ++ x ++ ":=" ++ e ++ "x")%string]=>
-                               (inr (update_map x l nilmap))/(update_map l v m')
-| CNilRec s m : s/m =["{}"]=> (inr nilmap)/m
-| CField s m e v x l m' : s/m =[e]=> (inr (update_map x l nilmap))/m' -> m' l = Some v
-                          -> s/m =[(e ++ "." ++ x)]=> v/m'
-| CAssign s m e x v m' l : s/m =[e]=> v/m' -> s x = Some l -> s/m =[(x ++ ":=" ++ e)]=> v/(update_map l v m')
-| CSeq s m1 m2 m3 e1 e2 v1 v2 : s/m1 =[e1]=> v1/m2 -> s/m2 =[e2]=> v2/m3 -> s/m1 =[(e1 ++ ";" ++ e2)]=>v2/m3
-| CLet s m1 m2 m3 e1 e2 v1 v2 l x :
-  s/m1 =[e1]=> v1/m2 -> (update_map x l s)/(update_map l v1 m2) =[e2]=> v2/m3
-  -> m1 l = None -> m2 l = None -> s/m1 =[("let" ++ x ++ e1 ++ e2)]=> v2/m3
-where "s '/' m '=[' e ']=>' v '/' m'" := (comp s m e v m').
+                               (inr (update_map x l nilmap))/(update_map l v m')*(l::ls)
+| CNilRec s m : s/m =["{}"]=> (inr nilmap)/m*nil
+| CField s m e v x l m' ls : s/m =[e]=> (inr (update_map x l nilmap))/m'*ls -> m' l = Some v
+                          -> s/m =[(e ++ "." ++ x)]=> v/m'*(l::ls)
+| CAssign s m e x v m' l ls : s/m =[e]=> v/m'*ls -> s x = Some l -> s/m =[(x ++ ":=" ++ e)]=> v/(update_map l v m')*(l::ls)
+| CSeq s m1 m2 m3 e1 e2 v1 v2 ls1 ls2 : s/m1 =[e1]=> v1/m2*ls1 -> s/m2 =[e2]=> v2/m3*ls2 -> s/m1 =[(e1 ++ ";" ++ e2)]=>v2/m3*(ls1++ls2)
+| CLet s m1 m2 m3 e1 e2 v1 v2 l x ls1 ls2 :
+  s/m1 =[e1]=> v1/m2*ls1 -> (update_map x l s)/(update_map l v1 m2) =[e2]=> v2/m3*ls2
+  -> m1 l = None -> m2 l = None -> s/m1 =[("let" ++ x ++ e1 ++ e2)]=> v2/m3*(l :: ls1 ++ ls2)
+where "s '/' m '=[' e ']=>' v '/' m' '*' l" := (comp s m e v m' l).
                                                 
+Inductive reach (s : env) (m : mem) : loc -> Prop :=
+| varAc x l : s x = Some l -> reach s m l
+| pntAc x l l' : reach s m l -> m l = Some (inr (update_map x l' nilmap)) -> reach s m l'.
+
+Theorem safety s m e v m' x : forall ls l, (s/m =[e]=> v/m' * ls -> In l ls -> m l = Some x -> reach s m l).
 
 
 
 
-Definition loc := nat.
-
-
-Definition val := 
