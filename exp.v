bc62184fd534773fde4c4f2cd38d834c7c869187
Jaehyung Lee <jaehyung.lee@sf.snu.ac.kr>
2022-03-28 13:29:54 +0900

    To consider interface is very important

--- exp.v
+++ exp.v
@@ -30,7 +30,14 @@ Coercion emb2 : rec >-> val.
 Hypothesis loc_dec : forall l l' : loc, {l=l'} + {l<>l'}.
 Definition int_of_string := NilEmpty.int_of_string.
 
-Class finmap 
+Class finmap (A B : Type) :=
+  { nilmap : A -> option B;
+    update_map : A -> B -> (A -> option B) -> (A -> option B);
+    update_well : forall a b f, (update_map a b f) a = Some b;
+    update_shadow : forall a b c f, (update_map a c (update_map a b f)) = update_map a c f;
+    update_change : forall a b c d f, a <> c -> (update_map a b (update_map c d f)) = (update_map c d (update_map a b f))}.
+Context `{FSL : finmap string loc}.
+Context `{FLV : finmap loc val}.
 
 Reserved Notation
          "s ',' m '=[' e ']=>' v ',' m'"
@@ -40,12 +47,16 @@ Inductive comp : env -> mem -> string -> val -> mem -> Prop :=
 | CInt s m e n : int_of_string e = Some n -> comp s m e n m
 | CVar s m x l v : s x = Some l -> m l = Some v -> comp s m x v m
 | CRec s m x e l v m' : comp s m e v m' -> m l = None -> m' l = None
-                        -> comp s m ("{" ++ x + ":=" ++ e ++ "x")%string (inr (fun y : string => if string_dec x y then Some l else None)) (fun y => if loc_dec l y then Some v else m' y)
-| CNilRec s m : comp s m Nil (inr (fun _ => None)) m
-| CField s m e v x l m' : comp s m e (inr (fun y => if string_dec x y then Some l else None)) m' -> m' l = Some v -> comp s m (Field e x) v m'
-| CAssign s m e x v m' l : comp s m e v m' -> s x = Some l -> comp s m (Assgn x e) v m'
-| CSeq s m1 m2 m3 e1 e2 v1 v2 : comp s m1 e1 v1 m2 -> comp s m2 e2 v2 m3 -> comp s m1 (Seq e1 e2) v2 m3
-| CLet s m1 m2 m3 e1 e2 : comp s m1 e1 v1 m2 -> comp 
+                        -> comp s m ("{" ++ x ++ ":=" ++ e ++ "x")%string
+                               (inr (update_map x l nilmap)) (update_map l v m')
+| CNilRec s m : comp s m "{}" (inr nilmap) m
+| CField s m e v x l m' : comp s m e (inr (update_map x l nilmap)) m' -> m' l = Some v
+                          -> comp s m (e ++ "." ++ x) v m'
+| CAssign s m e x v m' l : comp s m e v m' -> s x = Some l -> comp s m (x ++ ":=" ++ e) v (update_map l v m')
+| CSeq s m1 m2 m3 e1 e2 v1 v2 : comp s m1 e1 v1 m2 -> comp s m2 e2 v2 m3 -> comp s m1 (e1 ++ ";" ++ e2) v2 m3
+| CLet s m1 m2 m3 e1 e2 v1 v2 l x :
+  comp s m1 e1 v1 m2 -> comp (update_map x l s) (update_map l v1 m2) e2 v2 m3
+  -> m1 l = None -> m2 l = None -> comp s m1 ("let" ++ x ++ e1 ++ e2) v2 m3.
                                                  
 
 
